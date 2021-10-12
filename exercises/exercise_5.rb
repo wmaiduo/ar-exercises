8cb5b863b0581119aa82992d56462f3a500c37f6
Mai Duo Wu <wmaiduo@gmail.com>
2021-10-12 08:09:51 +0000

    finished exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,6 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+puts Store.sum(:annual_revenue)
+puts Store.average(:annual_revenue)
+puts Store.where(annual_revenue: 1000000..Float::INFINITY).count
