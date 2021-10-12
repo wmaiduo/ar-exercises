c15ce7d9f0f42aaed8cf174e5ad6751601ac7b2b
Mai Duo Wu <wmaiduo@gmail.com>
2021-10-12 08:46:22 +0000

    finished exercise 7

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -9,4 +9,17 @@ require_relative './exercise_6'
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, inclusion: 40..200
+  validates_associated :store
+end
+
+class Store
+  validates :name, length: {minimum: 3}
+  validates :annual_revenue, inclusion: 0..Float::INFINITY
+end 
+puts "Give a store name"
+store_name = gets.chomp
+p Store.create(name: store_name)
